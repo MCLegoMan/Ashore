aa302892bcb5ec752690237b9652be837635638a
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-07-10 02:16:36 +1200

    Update to 1.21

--- common/src/main/java/com/mclegoman/ashore/mixin/BoatEntityMixin.java
+++ common/src/main/java/com/mclegoman/ashore/mixin/BoatEntityMixin.java
@@ -27,51 +27,51 @@ public abstract class BoatEntityMixin extends Entity {
 	@Shadow private double waterLevel;
 	@Shadow private float yawVelocity;
 	@Shadow
-	public abstract float getWaterLevelAbove();
-	@Shadow
 	private double fallVelocity;
-	@Shadow
-	private float landFriction;
+	@Shadow private float nearbySlipperiness;
+	@Shadow public abstract float getWaterHeightBelow();
 	public BoatEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 	@Inject(method = "updateVelocity", at = @At("HEAD"), cancellable = true)
 	private void updateVelocity(CallbackInfo ci) {
-		double d = -0.03999999910593033;
-		double e = this.hasNoGravity() ? 0.0 : d;
-		double f = 0.0;
+		double d = -this.getFinalGravity();
+		double e = 0.0;
 		this.velocityDecay = 0.05F;
 		if (this.lastLocation == BoatEntity.Location.IN_AIR && this.location != BoatEntity.Location.IN_AIR && this.location != BoatEntity.Location.ON_LAND) {
 			this.waterLevel = this.getBodyY(1.0);
-			this.setPosition(this.getX(), (double)(this.getWaterLevelAbove() - this.getHeight()) + 0.101, this.getZ());
-			this.setVelocity(this.getVelocity().multiply(1.0, 0.0, 1.0));
-			this.fallVelocity = 0.0;
+			double f = (double)(this.getWaterHeightBelow() - this.getHeight()) + 0.101;
+			if (this.getWorld().isSpaceEmpty(this, this.getBoundingBox().offset(0.0, f - this.getY(), 0.0))) {
+				this.setPosition(this.getX(), f, this.getZ());
+				this.setVelocity(this.getVelocity().multiply(1.0, 0.0, 1.0));
+				this.fallVelocity = 0.0;
+			}
 			this.location = BoatEntity.Location.IN_WATER;
 		} else {
 			if (this.location == BoatEntity.Location.IN_WATER) {
-				f = (this.waterLevel - this.getY()) / (double)this.getHeight();
+				e = (this.waterLevel - this.getY()) / (double)this.getHeight();
 				this.velocityDecay = 0.6F;
 			} else if (this.location == BoatEntity.Location.UNDER_FLOWING_WATER) {
-				e = -7.0E-4;
+				d = -7.0E-4;
 				this.velocityDecay = 0.6F;
 			} else if (this.location == BoatEntity.Location.UNDER_WATER) {
-				f = 0.009999999776482582;
+				e = 0.009999999776482582;
 				this.velocityDecay = 0.45F;
 			} else if (this.location == BoatEntity.Location.IN_AIR) {
 				this.velocityDecay = 0.9F;
 			} else if (this.location == BoatEntity.Location.ON_LAND) {
-				this.velocityDecay = this.landFriction > 0.9F ? this.landFriction : 0.9F;
-				if (this.getPrimaryPassenger() instanceof PlayerEntity) {
-					this.landFriction /= 2.0F;
+				this.velocityDecay = Math.max(this.nearbySlipperiness, 0.9F);
+				if (this.getControllingPassenger() instanceof PlayerEntity) {
+					this.nearbySlipperiness /= 2.0F;
 				}
 			}
-			this.velocityDecay = Math.min(this.velocityDecay, 1.0F);
+
 			Vec3d vec3d = this.getVelocity();
-			this.setVelocity(vec3d.x * (double)this.velocityDecay, vec3d.y + e, vec3d.z * (double)this.velocityDecay);
+			this.setVelocity(vec3d.x * (double)this.velocityDecay, vec3d.y + d, vec3d.z * (double)this.velocityDecay);
 			this.yawVelocity *= this.velocityDecay;
-			if (f > 0.0) {
+			if (e > 0.0) {
 				Vec3d vec3d2 = this.getVelocity();
-				this.setVelocity(vec3d2.x, (vec3d2.y + f * 0.06153846016296973) * 0.75, vec3d2.z);
+				this.setVelocity(vec3d2.x, (vec3d2.y + e * (this.getGravity() / 0.65)) * 0.75, vec3d2.z);
 			}
 		}
 		ci.cancel();
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Ashore (Quilt/Fabric/NeoForge)  
+Ashore is a Minecraft mod that switches boat velocity, making them slower on water and faster on land.    
+  
+## Mod Requirements  
+- [Quilt Loader](https://quiltmc.org/) (Recommended)  
+  - [QSL](https://modrinth.com/mod/qsl)  
+- [Fabric Loader](https://fabricmc.net/)  
+  - [Fabric API](https://modrinth.com/mod/fabric-api)  
+- [NeoForge](https://neoforged.net/)  
+  
+## FAQ  
+- **I've got another mod that changes boat behavior, is this compatible?**  
+  - Ashore has only been tested with a few other boat related mods. Of those tested, all have been compatible. That being said, there may be incompatibilities we don't know of.  
+- **How does block slipperiness affect this mod?**  
+  - Ashore uses the higher value of `0.9F` or `landFriction`. This should result in the same speed as vanilla for slippery vanilla blocks (e.g Blue Ice).  
+- **Can I use this in a modpack or video?**  
+  - Yes, you can use this mod in modpacks or videos.  
+- **Will you port this mod to 1.x.x?**  
+  - I am unlikely to port to specific versions, as this mod was made for fun. However, other developers are free to port this mod, as long as they adhere to the conditions of the LGPL-3.0-or-later license.  
+  
+## Known Compatible Mods  
+**These mods have been tested using Ashore `1.0.0`, Quilt Loader `0.21.1-beta.2`, and Minecraft `1.20.2`.**    
+*This list is **NOT** a complete list of all known boat mods.*  
+- [Boat Hud](https://modrinth.com/mod/boathud)  
+- [Realistic Rowing](https://modrinth.com/mod/realistic-rowing)  
+- [Ice Boat nerf](https://modrinth.com/mod/ice-boat-nerf)  
+- [Boat Item View](https://modrinth.com/mod/boat-item-view)  
+- [This boat is MINE!](https://modrinth.com/mod/this-boat-is-mine)  
+- [Boat Fall](https://modrinth.com/mod/boat-fall)  
+  
